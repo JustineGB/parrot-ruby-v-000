7f1153d2634f6fcce5a1e03d5af990b22ac4335b
Justine Bricknell <justinegayton@gmail.com>
2018-06-20 15:00:42 +0000

    automatically backed up by learn

--- parrot.rb
+++ parrot.rb
@@ -1,5 +1,5 @@
-def parrot("Pretty bird!")
-  puts "Squawk!"
-  return "Squawk!"
+def parrot(phrase = "Squawk!")
+  puts #{phrase}
+  return #{phrase}
 end
 # BOTH returns the phrase & puts the phrase
